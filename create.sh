09b75ab1668530faaffc0682156abd83b1592eeb
caplio <github@snugplace.net>
2013-05-31 12:37:03 +0900

    change dir name ramdisk

--- create.sh
+++ create.sh
@@ -4,24 +4,24 @@
 ##
 
 ## delete *.ko files
-rm ./boot.img-ramdisk/lib/modules/*
-
+rm ./ramdisk/lib/modules/*
+tar zxvf ./ramdisk.tgz ./
 # copy *.ko file to <ramdisk>/lib/modules
-find -name '*.ko' ! -path "*boot.img-ramdisk*" -exec cp -av {} ./boot.img-ramdisk/lib/modules \;
+find -name '*.ko' ! -path "*boot.img-ramdisk*" -exec cp -av {} ./ramdisk/lib/modules \;
 
 # copy prima_wlan_ko to prima_wlan.ko
-#cp -av ./boot.img-ramdisk/vendor/bcmdhd.ko ./boot.img-ramdisk/lib/modules/bcmdhd.ko
+#cp -av ./ramdisk/vendor/bcmdhd.ko ./ramdisk/lib/modules/bcmdhd.ko
 
 # ramdisk copy to tmp dir
 mkdir tmp
-cp -pr ./boot.img-ramdisk ./tmp/boot.img-ramdisk
+cp -pr ./ramdisk ./tmp/ramdisk
 # rm .gitignore
-find ./tmp/boot.img-ramdisk -name '*.gitignore' -print | xargs rm
-chmod 750 ./tmp/boot.img-ramdisk/init.*
+find ./tmp/ramdisk -name '*.gitignore' -print | xargs rm
+chmod 750 ./tmp/ramdisk/init.*
 
 # create boot image >> output dir
-./build-tools/repack-bootimg.pl arch/arm/boot/zImage ./tmp/boot.img-ramdisk ./output/boot`date +%m%d`-`cat .version`.img
+./build-tools/repack-bootimg.pl arch/arm/boot/zImage ./tmp/ramdisk ./output/boot`date +%m%d`-`cat .version`.img
 
 # delete tmp dir
-rm -r ./tmp/boot.img-ramdisk ./tmp
+rm -r ./tmp/ramdisk ./tmp
 echo "output to ./output/boot`date +%m%d`-`cat .version`.img"
